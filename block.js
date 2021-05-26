d40e8b365c3765703422d88b4c62e95aaec250c5
sunanda4 <83966070+sunanda4@users.noreply.github.com>
2021-05-26 21:02:45 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,24 @@
+class Block{
+constructor(x,y,width,height) {
+var options={
+restituition:0.4;
+friction:1.0;
+//isStatic:true
+
+}
+this.body=Bodies.rectangle(x,y,width,height,options);
+this.width=width;
+this.height=height;
+world.add(world,this.body);
+}
+display(){
+rectangle=this.body.angle;
+var pos=this.body.position;
+Push();
+translate(pos.x,pos.y);
+rotate(angle);
+rectMode(CENTER);
+rect(0,0,this.width,this.height);
+Pop();
+}
+}
